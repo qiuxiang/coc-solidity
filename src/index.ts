1923bea3861d156f0ad6a45eff89d62dc20b74e0
7c00 <i@7c00.cc>
2022-03-23 14:58:40 +0800

    Switch to new language server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { ExtensionContext, LanguageClient, services } from "coc.nvim";
-import { join } from "path";
 
 export async function activate(context: ExtensionContext): Promise<void> {
   const languageClient = new LanguageClient(
     "solidity",
     "Solidity Language Server",
     {
-      module: join(__dirname, "..", "..", "solidity-language-server"),
+      module: require.resolve("solidity-ls"),
       options: { execArgv: ["--inspect"] },
     },
     {
